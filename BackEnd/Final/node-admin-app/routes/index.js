ddcc8938a0ada5dbb0da19a09bb2d5f5fbdd4468
Yugyeong Park <bonbonly7@naver.com>
2024-07-23 17:53:00 +0900

    2024-07-23 실습 미션2 index.js에 로그인, 메인 routing 메소드를 추가하고 view 구현하기

--- BackEnd/Final/node-admin-app/routes/index.js
+++ BackEnd/Final/node-admin-app/routes/index.js
@@ -6,4 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+// 로그인 화면 요청/응답 라우팅 메소드
+router.get('/login', function(req, res) {
+  res.render('login.ejs');
+});
+
+
+// 로그인 정보 처리 라우팅 메소드
+router.post('/login', function(req, res) {
+  const userId = req.userId;
+  const password = req.password;
+
+  const login = {
+    userId,
+    password
+  }
+
+  res.redirect('/main');
+});
+
+
+// 메인 페이지 요청/응답 라우팅 메소드
+router.get('/main', function(req, res) {
+  res.render('main.ejs');
+});
+
 module.exports = router;
