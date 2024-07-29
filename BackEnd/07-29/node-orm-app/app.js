df6ca918f6d771624e0381cbb6f76b5f8bb74ea4
Yugyeong Park <bonbonly7@naver.com>
2024-07-29 16:01:33 +0900

    2024-07-29 몰입교육 Day6 기본 routes와 views 생성

--- BackEnd/07-29/node-orm-app/app.js
+++ BackEnd/07-29/node-orm-app/app.js
@@ -3,16 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// 레이아웃 노드 패키지 참조
+var expressLayouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// 개발자 정의 라우터 파일 참조 - 게시글 정보 관리
+var articleRouter = require('./routes/article');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// 모든 view 파일에 적용되는 레이아웃 view 파일 설정
+app.set('layout', 'layout.ejs'); // 전체 레이아웃 파일 지정
+app.set("layout extractScripts", true); // script 태그룰 지정하겠다.
+app.set("layout extractStyles", true); // style 태그를 지정하겠다.
+app.set("layout extractMetas", true); // meta 태그를 지정하겠다.
 
+// 노드 앱에 레이아웃 기능 추가 적용
+app.use(expressLayouts);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +32,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// articleRouter 라우터의 기본 주소 체계 정의
+// http://localhost:3000/article
+app.use('/article', articleRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
