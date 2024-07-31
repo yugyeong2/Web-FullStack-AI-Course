d9b4bbbe57ee73ae2ef7c69fca2323de9c055fa8
Yugyeong Park <bonbonly7@naver.com>
2024-07-31 09:51:25 +0900

    2024-07-30 몰입교육 Day7 index 라우터에 로그인 로직 추가

--- BackEnd/Final/node-admin-app/routes/index.js
+++ BackEnd/Final/node-admin-app/routes/index.js
@@ -1,9 +1,21 @@
+// index.js 라우터의 용도는 전체 웹사이트의 공통 기능에 대한 라우팅 기능을 제공한다.
+// 기본 접속 주소는 http://localhost:5001로 접근하게 app.js에서 설정되어 있다.
 var express = require('express');
 var router = express.Router();
 
-/* 임시 메인 페이지 요청과 응답 처리 라우팅 메소드 */
+// 관리자 암호를 단방향 암호화(해시 알고리즘)하기 위해 bcryptjs 패키지 반환
+var bcrypt = require('bcryptjs');
+
+// ORM DB 객체를 참조
+var db = require('../models/index');
+
+
+/*
+메인 페이지 요청과 응답 처리 라우팅 메소드
+root 페이지에 접근하면, login 페이지로 이동
+*/
 router.get('/', async(req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.redirect('/login');
 });
 
 
@@ -14,31 +26,66 @@ router.get('/', async(req, res, next) => {
 - 응답 결과: login.ejs view 페이지 반환
 */
 router.get('/login', async(req, res) => {
-  
+  // 아이디/암호가 일치하지 않는 경우, 다시 로그인 view를 전달하고,
+  // 로그인 view에 결과 메시지를 전달한다.
+  let resultMsg = { // 변경이 가능한 let
+    code: 400,
+    msg: "",
+  }
+
   // {layout:false}: login.ejs view 파일 렌더링 시 레이아웃 페이지를 적용하지 않는다.
-  res.render('login.ejs', {layout:false}, {resultMsg:''});
+  res.render('login', {layout:false, resultMsg});
 });
 
 
 /*
-- 관리자 로그인 정보 처리 요청/응답 라우팅 메소드
+- 관리자가 입력한 아이디/암호를 추출하여 실제 로그인 프로세스를 처리하는 라우팅 메소드
 - 요청 주소: http://localhost:5001/login
 - 요청 방식: POST
 - 응답 결과: /main 페이지로 이동
 */
 router.post('/login', async(req, res, next) => {
-  const userId = req.body.userId;
-  const password = req.body.password;
 
-  // id와 password 체크 후 결과 확인
-  const result = false;
+  // 아이디/암호가 일치하지 않는 경우, 다시 로그인 view를 전달하고,
+  // 로그인 view에 결과 메시지를 전달한다.
+  let resultMsg = { // 변경이 가능한 let
+    code: 400,
+    msg: "",
+  }
 
-  if(result) {
-    // 정상 로그인 시
-    res.redirect('/main');
+  // Step1: 관리자 아이디/암호를 추출한다.
+  const admin_id = req.body.admin_id;
+  const admin_password = req.body.admin_password;
+
+  // Step2: 동일한 괸리자 아이디 정보를 조회한다.
+  const admin = await db.Admin.findOne({
+    where:{admin_id:admin_id}
+  })
+
+  // Step3: DB에 저장된 암호와 관리자 입력 암호를 체크한다.
+  // 동일한 아이디가 존재하는 경우
+  if(admin) {
+    // DB에 저장된 암호와 관리자가 로그인 화면에서 입력한 암호와 일치하는지 체크
+    // Step4: 아이디/암호가 일치하면 메인 페이지로 이동하고, 그렇지 않으면 처리결과 data를 login.ejs에 연결한다.
+    if(await bcrypt.compare(admin_password, admin.admin_password)) { // bcrypt.compare('로그인 화면에서 전달된 암호', db에서 저장된 암호화된 문자열) 메소드는 암호가 같으면 true 반환, 다르면 false 반환
+      resultMsg.code = 200;
+      resultMsg.msg = "로그인 성공! 환영합니다."
+
+      res.render('home');
+
+    } else {
+      // 암호가 일치하지 않는 경우
+      resultMsg.code = 402;
+      resultMsg.msg = "암호가 일치하지 않습니다."
+
+      res.render('login', {layout:false, resultMsg});
+    }
   } else {
-    // 아이디 또는 암호가 틀리면 다시 로그인 페이지 반환
-    res.render('login.ejs', {resultMsg:'로그인 실패'})    
+    // 동일한 아이디가 없는 경우
+    resultMsg.code = 401;
+    resultMsg.msg = "동일한 아이디가 존재하지 않습니다."
+    
+    res.render('login', {layout:false, resultMsg});
   }
 });
 
@@ -49,8 +96,8 @@ router.post('/login', async(req, res, next) => {
 - 요청 방식: GET
 - 응답 결과: main.ejs view 페이지 반환
 */
-router.get('/main', async(req, res) => {
-  res.render('main.ejs');
+router.get('/home', async(req, res) => {
+  res.render('home.ejs');
 });
 
 
