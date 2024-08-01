6cac7422e7a4f70c28fa75724cf32efd2d7e380c
Yugyeong Park <bonbonly7@naver.com>
2024-08-01 10:07:07 +0900

    2024-07-31 몰입교육 Day8 app.js에 CORS 참조, 처리 추가

--- BackEnd/Final/node-chat-app/app.js
+++ BackEnd/Final/node-chat-app/app.js
@@ -4,10 +4,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// RESTful API 서비스 CORS 이슈 해결을 위한 CORS 패키지 참조하기
+const cors = require("cors");
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// 모든 웹사이트/모바일 프론트에서 REST API를 접근할 수 있게 허락한다.
+// app.use(cors());
+
+// 특정 도메인 주소만 허가
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    origin: ["http://localhost:5000", "https://naver.com"],
+  })
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
