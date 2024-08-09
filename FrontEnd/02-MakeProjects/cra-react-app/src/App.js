283b1d3b63a55e3d2156e3dd95037ba7c2d0f28a
Yugyeong Park <bonbonly7@naver.com>
2024-08-09 10:06:15 +0900

    2024-08-08 몰입교육 Day14 CRA 방식으로 리액트 프로젝트 생성

---
+++ FrontEnd/02-MakeProjects/cra-react-app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          CRA 방식의 리액트 프로젝트입니다.          
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
