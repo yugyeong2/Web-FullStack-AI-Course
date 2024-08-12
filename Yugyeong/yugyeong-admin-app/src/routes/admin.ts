7f5bdf3b672173cc4b52b8f320e6d1dbe9385955
Yugyeong Park <bonbonly7@naver.com>
2024-08-12 13:23:37 +0900

    2024-08-10 몰입교육 프로젝트 admin create 라우터를 DB에 연결

--- Yugyeong/yugyeong-admin-app/src/routes/admin.ts
+++ Yugyeong/yugyeong-admin-app/src/routes/admin.ts
@@ -18,22 +18,36 @@ router.get("/create", async (req, res) => {
   res.render("admin/create");
 });
 
-// 신규 admin 게시글 등록
+// 신규 admin 계정 등록
 router.post("/create", async (req, res) => {
-  const admin = {
-    admin_id: "test",
-    admin_password: "test user",
-    admin_name: "테스트 사용자",
-    email: "test@test.ac.kr",
-    telephone: "010-1234-5678",
-    dept_name: "테스트팀",
-    used_yn_code: 1,
-    reg_date: Date.now(),
-    reg_member_id: 1,
+  const admin_id  = req.body.adminID;
+  const admin_password = req.body.adminPassword;
+  const admin_name = req.body.adminName;
+  const admin_email = req.body.adminEmail;
+  const admin_birth = req.body.adminBirth;
+  const admin_telephone = req.body.adminTelephone;
+  const admin_gender = req.body.adminGender;
+  // const company_department = req.body.company_department;
+  // const account_status = req.body.account_status;
+
+  const createdAdmin = {
+    admin_id,
+    admin_password,
+    admin_name,
+    admin_email,
+    admin_birth,
+    admin_telephone,
+    admin_gender,
+    company_department: "test", // 나중에 create.ejs에 form 만들고 추가
+    account_status: 1, // 나중에 create.ejs에 form 만들고 추가
+    register_date: Date.now(),
+    register_index_id: 1, // 나중에 세션 만들고 수정
   };
 
-  const registeredAdmin = await db.Admin.create(admin);
-  res.redirect("/article/list");
+  const registeredAdmin = await db.Admin.create(createdAdmin);
+  console.log("생성된 신규 admin 계정:", registeredAdmin);
+
+  res.redirect("/admin/list");
 });
 
 // 기존 admin 게시글 수정
