ca547e9c2597e2fcac4af7768ac565e0e62cfd67
Yugyeong Park <bonbonly7@naver.com>
2024-08-16 13:18:05 +0900

    2024-08-16 몰입교육 Day19  next-rendering-app initial

---
+++ FrontEnd/06-Hooks-Rendering/next-rendering-app/pages/csr.tsx
@@ -0,0 +1,11 @@
+// CSR(Client Side Rendering) 방식으로 UI 표시하기
+// client.js 파일에 의해 웹브라우저 환경에서 해당 컴포넌트의 렌더링
+const CSR = () => {
+    return (
+        <div>
+            CSR(Client Side Rendering)
+        </div>
+    );
+}
+
+export default CSR;
