751c2d6e54249490eed027e0a229a9133063f65b
Yugyeong Park <bonbonly7@naver.com>
2024-08-19 17:09:45 +0900

    2024-08-19 몰입교육 Day20 Final 백엔드 게시글 등록 API 추가

--- BackEnd/Final/node-chat-app/routes/articleAPI.js
+++ BackEnd/Final/node-chat-app/routes/articleAPI.js
@@ -36,6 +36,99 @@ router.get('/list', async(req, res) => {
 
 
 /*
+ * 신규 게시글 등록 요청 및 응답처리 API 라우팅 메소드
+ * 호출 주소: http://localhost:5000/api/article/create
+ * 요청 방식: GET
+ * 응답 결과: 등록된 단일 게시글 데이터
+ */
+router.post('/create', async(req, res) => {
+    let apiResult = {
+        code: 400,
+        data: null,
+        message: "400: 요청 리소스가 없습니다."
+    };
+
+    try {
+        // Step1: 프론트엔드에서 전달한 데이터 추출
+        const board_type_code = req.body.board_type_code;
+        const title = req.body.title;
+        const contents = req.body.contents;
+        const uploadFile = req.body.file;
+    
+        // Step2: DB article 테이블에 저장할 JSON 데이터 생성
+        // Article 모델의 속성명과 데이터 속성명을 동일하게 작성해야 한다.
+        const article = {
+            board_type_code,
+            title,
+            article_type_code: 2, // 게시판 고유번호 - 2: 일반 사용자 게시판
+            contents,
+            view_count: 0, // 게시글 생성 시 조회수는 0
+            ip_address: req.headers["x-forwarded-for"] || req.connection.remoteAddress, // 사용자 IP 추출 -> 로컬 개발 환경인 경우 ::1로 나올 수 있다.
+            is_display_code: 1, // 게시 여부 - 1: 게시
+            reg_date: Date.now(),
+            reg_member_id: 1 // 추후 토큰에서 사용자 정보 추출
+        };
+        
+        // Step3: DB article 테이블에 신규 게시글 정보 등록 처리
+        const registedArticle = await db.Article.create(article);
+
+        // Step4: 처리 결과 값 프론트엔드에 반환
+        apiResult.code = 200;
+        apiResult.data = registedArticle;
+        apiResult.message = "200 OK";
+
+    } catch (error) {
+        apiResult.code = 500;
+        apiResult.data = null;
+        apiResult.message = "ServerERR: 자세한 내용은 백엔드에 문의해주세요."
+    }
+
+    res.json(apiResult);
+});
+
+
+router.get('/delete', async(req, res) => {
+    let apiResult = {
+        code: 400,
+        data: null,
+        message: "400: 요청 리소스가 없습니다."
+    }
+});
+
+
+/*
+ * 기존 게시글 수정 요청 및 응답처리 API 라우팅 메소드
+ * 호출 주소: http://localhost:5000/api/article/modify/1
+ * 요청 방식: GET
+ * 응답 결과: 단일 게시글 수정 결과 데이터
+ */
+router.get('/modify/:id', async(req, res) => {
+    const articleIdx = req.parmas.id;
+
+    let apiResult = {
+        code: 400,
+        data: null,
+        message: "400: 요청 리소스가 없습니다."
+    };
+
+    try {
+        const articles = await db.Article.findOne({where: {article_id: articleIdx}});
+        
+        apiResult.code = 200;
+        apiResult.data = articles;
+        apiResult.message = "200 OK"
+
+    } catch (error) {
+        apiResult.code = 500;
+        apiResult.data = null;
+        apiResult.message = "ServerERR: 자세한 내용은 백엔드에 문의해주세요."
+    }
+    
+    res.json(apiResult);
+});
+
+
+/*
  * 단일 게시글 목록 조회 요청 및 응답처리 API 라우팅 메소드
  * 호출 주소: http://localhost:5000/api/article/:id
  * 요청 방식: GET
@@ -45,7 +138,7 @@ router.get('/:id', async(req, res) => {
     let apiResult = {
         code: 400,
         data: null,
-        message: ""
+        message: "400: 요청 리소스가 없습니다."
     };
 
     try {
@@ -61,7 +154,7 @@ router.get('/:id', async(req, res) => {
     } catch (err) {
         apiResult.code = 500;
         apiResult.data = null;
-        apiResult.message = "Server Error"
+        apiResult.message = "ServerERR: 자세한 내용은 백엔드에 문의해주세요."
     }
 
     res.json(apiResult);
